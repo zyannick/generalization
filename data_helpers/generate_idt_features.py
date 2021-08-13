5630a5afdfb74b266b43db848b7569b199d55fdf
Yannick Zoetgnande <yannick.zoetgnande@univ-rennes1.fr>
2021-08-13 14:16:40 +0200

    maj

--- data_helpers/generate_idt_features.py
+++ data_helpers/generate_idt_features.py
@@ -5,6 +5,9 @@ import numpy as np
 import skvideo.io
 import densetrack
 from glob import glob
+from termcolor import colored
+from datetime import datetime
+from time import time
 
 
 
@@ -13,13 +16,19 @@ save_dir = 'five_fps_cme_sep_idt'
 
 
 def generate_files(video_file_name):
+    head, tail = os.path.split(video_file_name)
+    name = os.path.splitext(tail)[0]
+    print(colored( '{}  --> Starting {} at {}'.format( mp.current_process(), name, datetime.fromtimestamp(int(time()))), 'green'))
+    debut = time()
     video_frames = skvideo.io.vreader(fname=video_file_name, as_grey=True)
     video_gray = np.stack([np.reshape(x, x.shape[1:3])
                         for x in video_frames]).astype(np.uint8, copy=False)
     tracks = densetrack.densetrack(video_gray, adjust_camera=True)
-    head, tail = os.path.split(video_file_name)
-    name = os.path.splitext(tail)[0]
+    
     np.save(os.path.join(save_dir, name + '-traj'), tracks)
+
+    fin = time()
+    print(colored( '{} --> Ending {} at {} for a duration of  {} seconds'.format(mp.current_process(), name, datetime.fromtimestamp(int(time())), int(fin - debut)), 'blue'))
 
 
 
